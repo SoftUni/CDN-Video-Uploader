9aecbb55ebf16f1ab0668b842cba90a335507a44
Svetlin Nakov <github@nakov.com>
2020-11-10 23:16:31 +0200

    Added tasks and actions classes

---
+++ Data/TaskForExecution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDN_Video_Uploader.Data
+{
+    public class TaskForExecution
+    {
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public float PercentsDone { get; set; }
+        public string SourceFileName { get; set; }
+        public List<TaskAction> Actions { get; set; }
+    }
+}
